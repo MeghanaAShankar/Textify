161c934f6abde7f720976a6918e73d8db6f500bf
meghana as <Meghananikhi@gmail.com>
2020-10-29 14:10:21 +0530

    login-parent component

--- src/Register/login.js
+++ src/Register/login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-
+//parent component
 export default class Login extends Component {
     constructor(props) {
         super(props);
